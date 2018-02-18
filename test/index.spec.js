c60eb805b93f44aa2551493b3a44026fb479db5b
Pascal Duez <public@pascalduez.me>
2018-02-18 16:23:35 +0100

    Add unit tests

---
+++ test/index.spec.js
@@ -0,0 +1,47 @@
+const cases = require('jest-in-case');
+const { transform } = require('babel-core');
+
+cases(
+  'transforms',
+  ({ name, input }) => {
+    const { code } = transform(input, {
+      presets: [require('../')],
+    });
+
+    expect(code).toMatchSnapshot(name);
+  },
+  [
+    {
+      name: 'object rest spread',
+      input: `
+        const b = {...a}
+      `,
+    },
+    {
+      name: 'flow',
+      input: `
+        // @flow
+        type A = string
+        const a: A = 'hello'`,
+    },
+    {
+      name: 'react',
+      input: `
+      const A = () => <div>hello</div>
+      `,
+    },
+    {
+      name: 'class properties',
+      input: `
+        class A { a = () => 'hello' }
+      `,
+    },
+    {
+      name: 'decorators legacy',
+      input: `
+        @decorated
+        class A {}
+      `,
+    },
+  ]
+);
