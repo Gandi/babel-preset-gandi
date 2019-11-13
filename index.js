790c00bd47579476525d9209e2c7af5df74ec74f
Pascal Duez <public@pascalduez.me>
2019-11-13 19:57:57 +0100

    Add comment about Flow plugin order

--- index.js
+++ index.js
@@ -21,6 +21,13 @@
 /**
  * 1. Ensure all helpers are imported instead of inlined.
  *    See https://github.com/babel/babel/issues/9297#issuecomment-453750049
+ *
+ * 2. Plugins are run in the order defined below,
+ *    but they are run *before* presets.
+ *    So we need to make sure the Flow annotations are removed before
+ *    the class properties plugin.
+ *    This is to avoid the `undefined` properties issue with old Flow props
+ *    syntax: `class A { props: Props }`.
  */
 module.exports = (context, options) => {
   let envOpts = Object.assign(
@@ -48,7 +55,7 @@ module.exports = (context, options) => {
   ];
 
   let plugins = [
-    '@babel/plugin-transform-flow-strip-types',
+    '@babel/plugin-transform-flow-strip-types', // 2
     '@babel/plugin-proposal-optional-chaining',
     '@babel/plugin-proposal-nullish-coalescing-operator',
     ['@babel/plugin-proposal-decorators', { legacy: true }],
