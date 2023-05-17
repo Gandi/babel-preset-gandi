a6c958a05f7647bb0d8ac1ae313490ca8a42378e
Pascal Duez <public@pascalduez.me>
2023-05-17 12:25:18 +0200

    Add targets tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,7 @@
 const { transform } = require('@babel/core');
 const { toMatchFile } = require('jest-file-snapshot');
+const browserslist = require('browserslist');
+
 const preset = require('../');
 
 expect.extend({ toMatchFile });
@@ -192,4 +194,44 @@ test.each(cases)('node: %s', (name, input) => {
   });
 
   expect(code).toMatchFile();
+});
+
+test('targets: modern', () => {
+  let {
+    options: { targets },
+  } = transform('', {
+    targets: { browsers: 'defaults', esmodules: true },
+    presets: [[preset, { modules: false }]],
+  });
+
+  let browsers = browserslist('defaults and supports es6-module');
+
+  let result = [
+    '// Babel targets',
+    JSON.stringify(targets, null, 2),
+    '// Browerslist targets',
+    JSON.stringify(browsers, null, 2),
+  ].join('\n');
+
+  expect(result).toMatchFile();
+});
+
+test('targets: legacy', () => {
+  let {
+    options: { targets },
+  } = transform('', {
+    targets: { browsers: 'defaults' },
+    presets: [[preset, { modules: false }]],
+  });
+
+  let browsers = browserslist('defaults');
+
+  let result = [
+    '// Babel targets',
+    JSON.stringify(targets, null, 2),
+    '// Browerslist targets',
+    JSON.stringify(browsers, null, 2),
+  ].join('\n');
+
+  expect(result).toMatchFile();
 });
